0d6e2e31ba1d5265a39e069ccc9ce6107401be20
PankEcal <pankaj.ecal@gmail.com>
2023-03-24 18:42:31 +0530

    add requests logger

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 // import from modules
 const planetsRouter = require("./routes/planets/planets.router");
@@ -10,8 +11,9 @@ const app = express();
 
 // implement middlewares
 app.use(cors({ origin: "http://localhost:3000" })); // enable CORS for API access to frontend
+app.use(morgan("combined"));
 app.use(express.json()); // enable JSON parse
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public"))); // add static file serving
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
